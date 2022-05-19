30be3da4ecdd3ce65f492d80d7dd2e4db44c9bd5
Yazmin Perez <zoeperez333@gmail.com>
2022-05-19 16:36:50 -0700

    update volunteer class

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -3,20 +3,41 @@ class Volunteer
   attr_accessor :name, :project_id
 
   def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @project_id = attributes.fetch(:project_id)
+    @id = attributes.fetch(:id)
   end
 
-  def ==()
+  def ==(volunteer_to_compare)
+    self.name == volunteer_to_compare.name
   end
 
   def self.all
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    returned_volunteers.each do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id")
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+    end
+    volunteers
   end
 
   def save
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
+    @id = result.first.fetch("id").to_i
   end
 
   def self.find(id)
+    volunteer = DB.exec("SELECT * FROM volunteers WHERE id = #{id};").first
+    if volunteer
+    name = volunteer.fetch("name")
+    project_id = volunteer.fetch("project_id").to_i
+    id = volunteer.fetch("id").to_i
+    Volunteer.new({:name => name, :project_id => project_id, :id => id})
+    else
+      nil
+    end
   end
-
-  
-
 end
