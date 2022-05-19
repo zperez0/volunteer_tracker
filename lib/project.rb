c29400742436c5df3eff05c8f8e7766708a8824f
Yazmin Perez <zoeperez333@gmail.com>
2022-05-19 12:31:39 -0700

    update initialize()

--- lib/project.rb
+++ lib/project.rb
@@ -2,6 +2,8 @@ class Project
   attr_reader :id
   attr_accessor :title
 
-  def initialize()
+  def initialize(attributes)
+    @title = attributes.fetch(:title)
+    @id = attributes.fetch(:id)
   end
 end
