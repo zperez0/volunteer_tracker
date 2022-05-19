da3d54df63b1efdc86bd038368b33c78d60bfdf9
Yazmin Perez <zoeperez333@gmail.com>
2022-05-19 12:30:30 -0700

    test 1-2 good fail

---
+++ spec/project_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe Project do
+  describe '#title' do
+    it 'returns the project title' do
+      project = Project.new({:title => 'Teaching kids to code', :id => nil})
+      expect(project.title).to eq 'Teaching kids to code'
+    end
+  end
+
+  context '#id' do
+    it 'returns the id of the project before saving project' do
+      project = Project.new({:title => 'Teaching kids to code', :id => nil})
+      expect(project.id).to eq nil
+    end
+  end
+
+end
