2e29c8b490e857bf0997e9cabc6fb1842a80a12f
Yazmin Perez <zoeperez333@gmail.com>
2022-05-19 16:37:25 -0700

    all test are passing

--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -7,7 +7,7 @@ describe Volunteer do
       expect(test_volunteer.name).to eq 'Jane'
     end
   end
-
+  
   describe '#project_id' do
     it 'returns the project_id of the volunteer' do
       test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
