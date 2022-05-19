f2db7dcaa342069e1826cea3034c5b0d3b7622e9
Yazmin Perez <zoeperez333@gmail.com>
2022-05-19 16:11:59 -0700

    add empty intitalize(), ==(), all(), save(), find()

---
+++ lib/volunteer.rb
@@ -0,0 +1,22 @@
+class Volunteer
+  attr_reader :id
+  attr_accessor :name, :project_id
+
+  def initialize(attributes)
+  end
+
+  def ==()
+  end
+
+  def self.all
+  end
+
+  def save
+  end
+
+  def self.find(id)
+  end
+
+  
+
+end
