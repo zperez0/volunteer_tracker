0f4455956b821b30ad7ee804cd980caaf19998ca
Yazmin Perez <zoeperez333@gmail.com>
2022-05-19 13:49:03 -0700

    update save()

--- lib/project.rb
+++ lib/project.rb
@@ -23,6 +23,9 @@ class Project
   end
 
   def save
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first().fetch("id").to_i
   end
+
 
 end
