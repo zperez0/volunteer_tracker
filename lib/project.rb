6989407c81721e238d3e2dff2595c83b224e2668
Yazmin Perez <zoeperez333@gmail.com>
2022-05-20 11:48:21 -0700

    fix update() and test in order to get routing to work

--- lib/project.rb
+++ lib/project.rb
@@ -42,9 +42,9 @@ class Project
     Volunteer.find_by_project(self.id)
   end
 
-  def update(attr)
-    @title = attr.fetch(:title)
-    DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id}")
+  def update(title)
+    @title = title
+    DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
   end
 
   def delete
